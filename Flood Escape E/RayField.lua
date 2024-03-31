57a21770f4bc28f6690a14c963542da05065b06e
MuhXd <momo2010sofia@gmail.com>
2024-03-31 18:17:28 +0100

    Update

--- Flood Escape E/RayField.lua
+++ Flood Escape E/RayField.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
 	Name = "Flood Escape E",
