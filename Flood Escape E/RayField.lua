b928439cc7d34767bf2923c0168b98c9fc7e38de
MuhXd <momo2010sofia@gmail.com>
2024-03-31 19:12:33 +0100

    lo

--- Flood Escape E/RayField.lua
+++ Flood Escape E/RayField.lua
@@ -9,20 +9,18 @@ xp = 0
 
 btns:Button("Give XP", function()
 	local args = {
-		[1] = 10, --points (no matter)
+		[1] = 0, --points (no matter)
 		[2] = xp --(xp stuff)
 	}
 	
 	game:GetService("ReplicatedStorage").peressxp:FireServer(unpack(args))
 end)
 
-btns:Seperator()
 
 local sldr = sldrs:Slider("Give XP!", 0, 10000, 0, function(t)
 xp = t
 end)
 
-btns:Seperator()
 
 sldrs:Button("Change to 0", function()
 sldr:Change(0)
